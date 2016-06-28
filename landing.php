358a01a02bbe1699b9f980e61441c4334e5fc42f
Eugene Kwon <ekwon86@gmail.com>
2016-06-28 13:00:50 -0700

    up to date

---
+++ landing.php
@@ -0,0 +1,104 @@
+<?php
+session_start();
+//start your session
+//make sure to include your facebook credentials!
+require_once('credentials.php');
+//then you'll need to include the facebook sdk
+//require_once('php_oauth_facebook/libraries/facebook-php-sdk-v4-5.0.0/src/Facebook/autoload.php');
+
+require_once('library/facebook-php-sdk-v4-5.0.0/src/Facebook/autoload.php');
+//create a new facebook object
+$fb = new Facebook\Facebook([
+    'app_id'                => FACEBOOK_APP_ID,
+    'app_secret'            => FACEBOOK_SECRET,
+    'default_graph_version' => FACEBOOK_GRAPH_VERSION,
+]);
+
+//make a redirect helper handler
+
+$helper = $fb->getRedirectLoginHelper();
+//specify the permissions this app will need, putting them into an array
+$permissions = ['email','user_posts'];
+//generate the login url
+$loginUrl = $helper->getLoginUrl(SERVER_LANDING, $permissions);
+//echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
+?>
+
+    <h1 id="lunch_heading" class="col-xs-12">What's For Lunch?</h1>
+    <h3 class="landing-heading">Food Search That's Truly Simple</h3>
+    <h4 class="landing-heading">Find The Best Local Cuisine - Effortlessly</h4>
+
+    <div class="landing-container">
+         <?php $url = htmlspecialchars($loginUrl);
+if(empty($_SESSION["name"])){
+
+      echo "<button type='button' class='btn btn-lg' id='login-button' > <i 
+class='fa fa-facebook-official fa-lg facebook-icon' aria-hidden='true'></i>
+<a  href='". $url ."'  class='login-link'> Log in with Facebook </a></button>";
+}//if name is empty
+else {
+echo "<div id='welcome_user'>Welcome ".$_SESSION["name"]."</div>";
+}//else to welcome the user
+?>
+
+        <div class="modal fade" id="login" role="dialog">
+            <div class="modal-dialog">
+
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h5 class="modal-title">Please Log in with Facebook</h5>
+                    </div>
+                    <div class="modal-body col-xs-12">
+                        <form class="login-form col-xs-12">
+                            <div class="col-xs-2 login-fields login-icon"><i class="fa fa-user" aria-hidden="true"></i></div>
+                            <input type="username" placeholder="Username" class="col-xs-10 login-fields">
+                            <div class="col-xs-2 login-fields login-icon"><i class="fa fa-key" aria-hidden="true"></i></div>
+                            <input type="password" placeholder="Password" class="col-xs-10 login-fields">
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="col-xs-12 submit">Log In</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+<section class="dropdown-container">
+    <div class="dropdown">
+        <select name="one" class="dropdown-select">
+            <option value="">What Type?</option>
+            <option value="1">American</option>
+            <option value="2">Japanese</option>
+            <option value="3">Chinese</option>
+        </select>
+    </div>
+    <div class="dropdown">
+        <select name="one" class="dropdown-select">
+            <option value="">How far?</option>
+            <option value="1">&lt; 1 mile</option>
+            <option value="2">3 miles</option>
+            <option value="3">&gt; 5 miles</option>
+        </select>
+    </div>
+    <div class="dropdown">
+        <select name="one" class="dropdown-select">
+            <option value="">How much?</option>
+            <option value="1">$</option>
+            <option value="2">$$</option>
+            <option value="3">$$$</option>
+            <option value="4">$$$$</option>
+        </select>
+    </div>
+    <div class="lets-go">
+        <div id="lets-go-text">Let's Go!</div>
+    </div>
+</section>
+<!--     Random Selection Button-->
+    <div class="circle">
+      <div id="random">Pick For Me!</div>
+    </div>
+
+    <input type="text" placeholder="Please Enter ZIP Code" class="zip-input">
---
+++ search.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+ require_once("fs_lib/src/FoursquareApi.php");
+ require_once("credentials.php");
+
+if (empty($_SESSION["id"])) {
+   $insert_query = "INSERT INTO `users` (`fb_login`) VALUES ('false')";
+   $resultOfInsert = mysqli_query($conn, $insert_query);
+   $guest_id = mysqli_insert_id($conn);
+   $_SESSION["id"] = $guest_id;
+}
+$_POST['search_option']['option'] = 'random';
+$_POST['search_option']['category'] = 'burgers';
+$_POST["latitude"] = 33.532029;
+$_POST["longitude"] = -117.702148;
+$_POST['radius'] = '5000';
+$_SESSION['id'] = '484';
+$search_radius = $_POST['radius'];
+$id = $_SESSION['id'];
+////   Set client key and secret
+	$client_key = FOURSQUARE_CLIENT_ID;
+	$client_secret = FOURSQUARE_SECRET_ID;
+////  declare new foursquare class with client key and client secret
+	$foursquare = new FoursquareApi($client_key, $client_secret);
+////	 Searching for venues nearby
+	$endpoint = "venues/explore";
+////  Conditionals for determining choice
+   $params = [
+
+       "ll"=> $_POST['latitude'].",".$_POST['longitude'],
+       "intent"=>"browse",
+       "radius"=>$search_radius,
+       "venuePhotos"=>1
+   ];
+/////////random search
+   if (isset($_POST['search_option']['option']) && $_POST['search_option']['option'] == 'random'){
+	$params["query"]="food";
+   }
+   else {
+	$params["query"]= $_POST['search_option']['category'];
+   }
+   $response = $foursquare->GetPublic($endpoint , $params);
+   $venues = $foursquare->GetPublic($endpoint , $params, $POST=false);
+   $testme = json_decode( $venues);
+   $venues = $testme -> response -> groups[0]->items;
+
+        //send query to db to find user selected restaurant to compare with  4squre results
+   $fave_rest = [];
+   $optimizeQuery = "SELECT `selected_restaurant`, count(id) AS 'visited_count' FROM `user_interaction` WHERE `unique_assigned_id` = '$id' GROUP BY `selected_restaurant` ORDER BY `visited_count` DESC";
+
+   $favoriteRestaurant = mysqli_query($conn, $optimizeQuery);
+   if(mysqli_num_rows($favoriteRestaurant)>0){
+   	while($row = mysqli_fetch_assoc($favoriteRestaurant)) {
+        	array_push($fave_rest,$row);
+        }//while
+        $toAjax = json_encode($fave_rest);
+   }
+
+$search_result['favorite_restaurants'] = $fave_rest;
+$search_result['fourSquare_search_results'] = $venues;
+$result = json_encode($search_result);
+print($result);
+?>
